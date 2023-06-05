db43eb034d57d603411eb2fb9665125916a15ea8
vinay <vp@kadellabs>
2023-06-05 12:36:19 +0530

    lib file add

--- bin/cdk-pipeline-eb-demo.ts
+++ bin/cdk-pipeline-eb-demo.ts
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkPipelineEbDemoStack } from '../lib/eb-appln-stack';
+import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
 
 const app = new cdk.App();
-new CdkPipelineEbDemoStack(app, 'CdkPipelineEbDemoStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new CdkPipelineStack(app, 'CdkPipelineStack', {
+   env: { account: '354029710936', region: 'us-east-1' },
 });
+
+app.synth();
